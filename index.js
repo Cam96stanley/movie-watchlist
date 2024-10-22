6d49b65c764216a437d133cababaaa53214bc002
cam96stanley <cam96stanley@gmail.com>
2024-10-21 23:30:18 -0400

    Added js for rendering movies
    Created the render function to populate the movies
    details and added CSS to style the rendered
    movies.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
+const movieContainer = document.getElementById("movies-container");
 
 searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
@@ -21,5 +22,33 @@ function getMovies(searchedMovie) {
 function getMovieDetails(id) {
   fetch(`http://www.omdbapi.com/?apikey=f490edf1&i=${id}`)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      console.log(data);
+      render(data);
+    });
+}
+
+function render(data) {
+  movieContainer.innerHTML += `
+    <div class="movie-div">
+        <div>
+            <img class="movie-poster" src=${data.Poster}/>
+        </div>
+        <div class="movie-details">
+            <div class="title-details">
+                <p class="title">${data.Title} <span class="rating">⭐️ ${data.Ratings[0].Value}</span></p>
+            </div>
+            <div class="watch-details">
+                <p>${data.Runtime} ${data.Genre}</p>
+                <p><a href="#" id="watchlist-btn">
+                    <img src="assets/add-icon.png"/>
+                    Watchlist
+                </a></p>
+            </div>
+            <div class="summary-div">
+                <p class="summary">${data.Plot}</p>
+            </div>
+        </div>
+    </div>
+    `;
 }
